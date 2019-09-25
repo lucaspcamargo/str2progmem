7a49547b5a18cb9be0c76c06aa4275613f19a3af
Lucas Camargo <lucas@camargo.eng.br>
2019-09-25 15:37:31 -0300

    added example

---
+++ example/str_en.c
@@ -0,0 +1,9 @@
+#include "./str_en.h"
+
+const char str_en_blob[] PROGMEM = 
+"\x49\x20\x9d\x20\x4e\x59\x21\x00\x01\x02\xad\x0f\xff\x00"
+;
+
+const unsigned short str_en_offsets[] PROGMEM = {
+    0, 8};
+
